204dc1b1403fad4b5f58b022220f4d0640cf6387
ddub <calobodude@gmail.com>
2020-04-01 22:06:20 -0500

    Functional and tested ssignal and swait

--- system/ssignal.c
+++ system/ssignal.c
@@ -1,13 +1,47 @@
+/* signal.c - signal */
+
 #include <xinu.h>
 
-/*---------------------------------------------------------*/
-/* This is an empty shell for a system call named ssignal. */
-/*---------------------------------------------------------*/
-
-syscall ssignal(
-        sid32   semA,   
-        sid32   semB
-        )
+/*------------------------------------------------------------------------
+ *  signal  -  Signal a semaphore, releasing a process if one is waiting
+ *------------------------------------------------------------------------
+ */
+syscall	ssignal(
+	  sid32		sem1,		/* ID of semaphore to signal	*/
+	  sid32		sem2		/* ID of semaphore to signal	*/
+	)
 {
-    return OK;
+	intmask mask;			/* Saved interrupt mask		*/
+	struct	sentry *sem1ptr;		/* Ptr to sempahore table entry	*/
+	struct	sentry *sem2ptr;		/* Ptr to sempahore table entry	*/
+
+	mask = disable();
+	//if (isbadsem(sem)) {
+	if (isbadsem(sem1) || isbadsem(sem2) ) {
+		restore(mask);
+		return SYSERR;
+	}
+
+	if( sem1 == sem2 ) {
+		restore(mask);
+		return SYSERR;
+	}
+
+	sem1ptr= &semtab[sem1];
+	sem2ptr= &semtab[sem2];
+
+	//if (semptr->sstate == S_FREE) {
+	if (sem1ptr->sstate == S_FREE || sem2ptr->sstate == S_FREE ) {
+		restore(mask);
+		return SYSERR;
+	}
+
+	if ((sem1ptr->scount++) < 0) {	/* Release a waiting process */
+		ready(dequeue(sem1ptr->squeue));
+	}
+	if ((sem2ptr->scount++) < 0) {	/* Release a waiting process */
+		ready(dequeue(sem2ptr->squeue));
+	}
+	restore(mask);
+	return OK;
 }
--- system/swait.c
+++ system/swait.c
@@ -1,13 +1,75 @@
+/* wait.c - wait */
+
 #include <xinu.h>
 
-/*---------------------------------------------------------*/
-/* This is an empty shell for a system call named ssignal. */
-/*---------------------------------------------------------*/
-
-syscall swait(
-        sid32 semA,
-        sid32 semB
-        )
+/*------------------------------------------------------------------------
+ *  wait  -  Cause current process to wait on a semaphore
+ *------------------------------------------------------------------------
+ */
+syscall	swait(
+	  sid32		sem1,		/* Semaphore on which to wait  */
+	  sid32		sem2		/* Semaphore on which to wait  */
+	)
 {
-    return OK; 
+	intmask mask;			/* Saved interrupt mask		*/
+	struct	procent *prptr;		/* Ptr to process' table entry	*/
+	struct	sentry *sem1ptr;	/* Ptr to sempahore table entry	*/
+	struct	sentry *sem2ptr;	/* Ptr to sempahore table entry	*/
+
+	mask = disable();
+	//if (isbadsem(sem)) {
+	if( sem1 == sem2 ) {
+		restore(mask);
+		return SYSERR;
+	}
+
+	if ( isbadsem(sem1) || isbadsem(sem2)) {
+		restore(mask);
+		return SYSERR;
+	}
+
+	sem1ptr = &semtab[sem1];
+	sem2ptr = &semtab[sem2];
+	//if (semptr->sstate == S_FREE) {
+	if (sem1ptr->sstate == S_FREE || sem2ptr->sstate == S_FREE) {
+		restore(mask);
+		return SYSERR;
+	}
+
+	if( sem1ptr->scount > 0 && sem2ptr->scount > 0 ) {
+		--(sem1ptr->scount);
+		--(sem2ptr->scount);
+	} else if( sem1ptr->scount <= 0 ) {
+		--(sem1ptr->scount);
+		prptr = &proctab[currpid];
+		prptr->prstate = PR_WAIT;	// Set state to waiting
+		prptr->prsem = sem1;		// Record semaphore ID
+		enqueue(currpid,sem1ptr->squeue);// Enqueue on semaphore
+		resched();			//   and reschedule
+	} else if( sem2ptr->scount <= 0 ) {
+		--(sem2ptr->scount);
+		prptr = &proctab[currpid];
+		prptr->prstate = PR_WAIT;	// Set state to waiting
+		prptr->prsem = sem2;		// Record semaphore ID
+		enqueue(currpid,sem2ptr->squeue);// Enqueue on semaphore
+		resched();			//   and reschedule
+	} 
+	// Check that sem1->cnt > 0 && sem2->cnt > 0 { // easy case
+	//   Do similar stuff to regular wait() call)
+	// } case 2 sem1->cnt > 0 && sem2->cnt <=0 {
+	// } case 3 sem1->cnt <= 0 && sem2->cnt > 0 {
+	// } case 4 sem1->cnt <= 0 && sem2->cnt <= 0 {
+	// }
+	/*
+	if (--(semptr->scount) < 0) {		// If caller must block	
+		prptr = &proctab[currpid];
+		prptr->prstate = PR_WAIT;	// Set state to waiting
+		prptr->prsem = sem;		// Record semaphore ID
+		enqueue(currpid,semptr->squeue);// Enqueue on semaphore
+		resched();			//   and reschedule
+	}
+	*/
+
+	restore(mask);
+	return OK;
 }

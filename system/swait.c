9123ca4dca8c326e602cb29674279ec351664791
nsabata <50713955+nsabata@users.noreply.github.com>
2020-04-07 15:55:49 -0500

    Cleaned and commented ssignal and swait
    Cleaned code, Inserted comments, Added resched_cntl

--- system/swait.c
+++ system/swait.c
@@ -1,75 +1,80 @@
-/* wait.c - wait */
+/*----------------------------------------------*/
+// Name     : Nicholas Sabata and David Moore	//
+// Class    : 8530-001							//
+// Assignment # : 2								//
+// Due Date     : April 7th, 2020				//
+/*----------------------------------------------*/
+/* swait.c - swait */
 
 #include <xinu.h>
 
 /*------------------------------------------------------------------------
- *  wait  -  Cause current process to wait on a semaphore
+ * swait - Cause current process to wait on a semaphore
+ * "Simultaneous Wait"
  *------------------------------------------------------------------------
  */
 syscall	swait(
-	  sid32		sem1,		/* Semaphore on which to wait  */
-	  sid32		sem2		/* Semaphore on which to wait  */
+	  sid32 sem1,				/* Semaphore 1 on which to wait  */
+	  sid32 sem2				/* Semaphore 2 on which to wait  */
 	)
 {
-	intmask mask;			/* Saved interrupt mask		*/
-	struct	procent *prptr;		/* Ptr to process' table entry	*/
-	struct	sentry *sem1ptr;	/* Ptr to sempahore table entry	*/
-	struct	sentry *sem2ptr;	/* Ptr to sempahore table entry	*/
+	intmask mask;				/* Saved interrupt mask */
+	struct procent *prptr;		/* Ptr to process' table entry */
+	struct sentry *sem1ptr;		/* Ptr to semaphore 1 table entry */
+	struct sentry *sem2ptr;		/* Ptr to semaphore 2 table entry */
 
+	// First disable interrupts
 	mask = disable();
-	//if (isbadsem(sem)) {
+	
+	// Verify that semaphore args are reasonable (failure returns SYSERR)
+	// (Semaphore args should not be the same)
 	if( sem1 == sem2 ) {
 		restore(mask);
 		return SYSERR;
 	}
-
-	if ( isbadsem(sem1) || isbadsem(sem2)) {
+	// (Semaphore args should exist and be allocated)
+	if ( isbadsem(sem1) || isbadsem(sem2) ) {
 		restore(mask);
 		return SYSERR;
 	}
 
+	// Assign semaphore pointers
 	sem1ptr = &semtab[sem1];
 	sem2ptr = &semtab[sem2];
-	//if (semptr->sstate == S_FREE) {
-	if (sem1ptr->sstate == S_FREE || sem2ptr->sstate == S_FREE) {
+	
+	// Check if either semaphore is currently in the free state
+	if ( sem1ptr->sstate == S_FREE || sem2ptr->sstate == S_FREE ) {
 		restore(mask);
 		return SYSERR;
 	}
-
+	
+	// Check if both semaphores have a count greater than zero
 	if( sem1ptr->scount > 0 && sem2ptr->scount > 0 ) {
-		--(sem1ptr->scount);
-		--(sem2ptr->scount);
-	} else if( sem1ptr->scount <= 0 ) {
-		--(sem1ptr->scount);
+		--(sem1ptr->scount);				/* Decrement sem 1 count */
+		--(sem2ptr->scount);				/* Decrement sem 2 count */
+		
+	// Case where sem 1 count is not greater than zero
+	// (Process will block in the wait state)
+	} else if( sem1ptr->scount <= 0 ) { 	/* If caller must block	*/
+		--(sem1ptr->scount);				/* Decrement sem 1 count */
 		prptr = &proctab[currpid];
-		prptr->prstate = PR_WAIT;	// Set state to waiting
-		prptr->prsem = sem1;		// Record semaphore ID
-		enqueue(currpid,sem1ptr->squeue);// Enqueue on semaphore
-		resched();			//   and reschedule
-	} else if( sem2ptr->scount <= 0 ) {
-		--(sem2ptr->scount);
+		prptr->prstate = PR_WAIT;			/* Set state to waiting	*/
+		prptr->prsem = sem1;				/* Record semaphore ID */
+		enqueue(currpid,sem1ptr->squeue);	/* Enqueue on semaphore */
+		resched();							/* Reschedule */
+		
+	// Case where sem 2 count is not greater than zero
+	// (Process will block in the wait state)
+	} else if( sem2ptr->scount <= 0 ) { 	/* If caller must block	*/
+		--(sem2ptr->scount);				/* Decrement sem 2 count */
 		prptr = &proctab[currpid];
-		prptr->prstate = PR_WAIT;	// Set state to waiting
-		prptr->prsem = sem2;		// Record semaphore ID
-		enqueue(currpid,sem2ptr->squeue);// Enqueue on semaphore
-		resched();			//   and reschedule
-	} 
-	// Check that sem1->cnt > 0 && sem2->cnt > 0 { // easy case
-	//   Do similar stuff to regular wait() call)
-	// } case 2 sem1->cnt > 0 && sem2->cnt <=0 {
-	// } case 3 sem1->cnt <= 0 && sem2->cnt > 0 {
-	// } case 4 sem1->cnt <= 0 && sem2->cnt <= 0 {
-	// }
-	/*
-	if (--(semptr->scount) < 0) {		// If caller must block	
-		prptr = &proctab[currpid];
-		prptr->prstate = PR_WAIT;	// Set state to waiting
-		prptr->prsem = sem;		// Record semaphore ID
-		enqueue(currpid,semptr->squeue);// Enqueue on semaphore
-		resched();			//   and reschedule
+		prptr->prstate = PR_WAIT;			/* Set state to waiting	*/
+		prptr->prsem = sem2;				/* Record semaphore ID */
+		enqueue(currpid,sem2ptr->squeue);	/* Enqueue on semaphore */
+		resched();							/* Reschedule */
 	}
-	*/
 
+	// Interrupt mask is restored and process returns from system call
 	restore(mask);
 	return OK;
 }
